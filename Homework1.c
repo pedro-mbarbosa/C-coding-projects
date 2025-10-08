0f13acb6e9ea3721baeb222c0e52cb5e6726d7b6
pedro-mbarbosa <163596225+pedro-mbarbosa@users.noreply.github.com>
2025-10-08 16:17:42 -0400

    Add files via upload

---
+++ Homework1.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+
+int main() {
+
+	printf("Hi, my name is Pedro Maciel Barbosa and I enjoy fishing and soccer\n");
+
+	int x = 10;
+	int y = 3;
+	double z = (double) x / y;
+	
+	printf("%f", z);
+}
